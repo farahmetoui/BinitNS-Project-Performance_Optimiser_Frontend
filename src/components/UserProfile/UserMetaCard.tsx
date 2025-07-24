3ab383cee2060db895dc2f100d9ccd4d0258eb3c
farahmetoui <farah.metoui@esprit.tn>
2025-07-24 19:48:01 +0200

    changestsconfig

--- src/components/UserProfile/UserMetaCard.tsx
+++ src/components/UserProfile/UserMetaCard.tsx
@@ -1,8 +1,3 @@
-import { useModal } from "../../hooks/useModal";
-import { Modal } from "../ui/modal";
-import Button from "../ui/button/Button";
-import Input from "../form/input/InputField";
-import Label from "../form/Label";
 
   type UserDetails = {
     userName: string;  
@@ -11,12 +6,8 @@ import Label from "../form/Label";
 
 };
 export default function UserMetaCard({userName,role}: UserDetails) {
-  const { isOpen, openModal, closeModal } = useModal();
-  const handleSave = () => {
-    // Handle save logic here
-    console.log("Saving changes...");
-    closeModal();
-  };
+ 
+
   return (
     <>
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
