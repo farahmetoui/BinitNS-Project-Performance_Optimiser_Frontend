a78f72fbe5eaa35298efa6da56deb9d498d5a950
farahmetoui <farah.metoui@esprit.tn>
2025-07-28 00:47:49 +0200

    fixMain

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,6 @@ createRoot(document.getElementById("root")!).render(
           <BrowserRouter>
             <App />
           </BrowserRouter>
-        <App />
         </AuthProvider>
       </AppWrapper>
     </ThemeProvider>
